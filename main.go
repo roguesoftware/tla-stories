b2b0704b42487b9ee1f2059ae6d97603d8ed5f31
Brian Martin <bri365@gmail.com>
2020-06-14 22:36:30 -0500

    load initial values from json file

--- main.go
+++ main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net"
+	"os"
 
 	"google.golang.org/grpc"
 
 	pb "github.com/roguesoftware/tla-proto"
 )
 
-const (
-	port = ":50506"
-)
+const port = ":50506"
+
+var initialStories []*pb.StoryItem
 
 type server struct {
 	pb.UnimplementedStoryServiceServer
@@ -26,34 +29,32 @@ func (s *server) GetStories(ctx context.Context, in *pb.StoryRequest) (*pb.Story
 	log.Printf("Received: %v %v %v", ids, locationIds, userIds)
 
 	var stories []*pb.StoryItem
-	var story pb.StoryItem
-
-	story.Id = "123456-abcdef"
-	story.LocationId = "loc-123"
-	story.Title = "City Hall"
-	story.Story = "This is a story about a city with a hall"
-
-	stories = append(stories, &story)
-
-	story.Id = "923456-kbcdef"
-	story.LocationId = "loc-123"
-	story.Title = "City Hall"
-	story.Story = "An interesting debate occurred here in 2006"
-
-	stories = append(stories, &story)
+	stories = initialStories[1:2]
 
 	return &pb.StoryReply{Stories: stories}, nil
 }
 
 func main() {
+	// load initial votes
+	fileName := "stories.json"
+	jsonFile, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalf("Error opening %v: %v", fileName, err)
+	}
+	defer jsonFile.Close()
+
+	jsonBytes, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(jsonBytes, &initialStories)
+
+	// create listener
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("Failed to listen: %v", err)
 	}
 
 	s := grpc.NewServer()
 	pb.RegisterStoryServiceServer(s, &server{})
-	log.Printf("Registered story server")
+	log.Printf("Registered story server with %v stories", len(initialStories))
 	if err := s.Serve(lis); err != nil {
 		log.Fatal(s.Serve(lis))
 	}
